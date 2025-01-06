80d9bff28e66bbc06700d22d0211d8f708246efd
gustavocanhan <gustavocanhan@gmail.com>
2025-01-06 18:55:43 -0300

    primeiro commit de 2025

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,10 +1,24 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {
+                  restaurante.categoria} | {restaurante.ativo}')
 
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
+restaurante_praca = Restaurante('Pizzaria', 'Pizza')
+restaurante_pizza = Restaurante('Pizzaria Saleta ', 'Pizza')
+restaurante_mc = Restaurante(nome='Mc Donalds', categoria='FastFood')
 
-print(restaurante_praca.nome)
+Restaurante.listar_restaurantes()
